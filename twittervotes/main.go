0bca9672db4145094214e46ec170bf88876bd56c
su-kun1899 <higedrum.coz@gmail.com>
2017-12-03 12:51:33 +0900

    5.3.2 read from MongDB

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,5 +1,36 @@
 package main
 
+import mgo "gopkg.in/mgo.v2"
+import "log"
+
 func main() {
 
+}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイアル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
 }
