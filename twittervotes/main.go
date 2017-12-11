23fe15c35d9e6dc9a8fe829b4fcc336e421ae540
su-kun1899 <higedrum.coz@gmail.com>
2017-12-11 21:59:13 +0900

    5.3.5 quietly start and stop

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,11 +1,57 @@
 package main
 
-import mgo "gopkg.in/mgo.v2"
-import "log"
-import "github.com/bitly/go-nsq"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/bitly/go-nsq"
+	mgo "gopkg.in/mgo.v2"
+)
 
 func main() {
+	var stoplock sync.Mutex
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("停止します...")
+		stopChan <- struct{}{}
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
+	if err := dialdb; err != nil {
+		log.Fatalln("MongDBへのダイヤルに失敗しました: ", err)
+	}
+	defer closedb()
+
+	// 処理を開始します
+	votes := make(chan string) // 投票結果のためのチャネル
+	publisherStoppedChan := publishVotes(votes)
+	twitterStoppedChan := startTwitterStream(stopChan, votes)
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			closeConn()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				break
+			}
+			stoplock.Unlock()
+		}
+	}()
+	<-twitterStoppedChan
+	close(votes)
+	<-publisherStoppedChan
 }
 
 var db *mgo.Session
